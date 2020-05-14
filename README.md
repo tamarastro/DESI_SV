b9459b78aff7e54e96b377650fdb0fbc40ec7c58
Tamara Davis <tamarad@physics.uq.edu.au>
2020-05-14 13:35:51 +1000

    Fairly big cleanup.

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # DESI_SV
-Repository for code relating to Dark Energy Spectroscopic Instrument (DESI) survey validation and visual inspection.  This is currently just a place for me to play and edit codes, not a final polished version that would be useful to others.  
-WARNING: Ugly code lives here (hopefully the ugly part is temporary).
+Repository for code relating to Dark Energy Spectroscopic Instrument (DESI) survey validation and visual inspection.  
+To merge VI you:
+1. Run VI_merge_sets_v1.py (changing the filenames in the code to the ones you want to merge)
+2. Run VI_merge_merged_files.py (which merges all of the output of VI_merge_sets_v1.py and generates a truth table)
+3. Run VI_merge_plots.py (to get some diagnostic plots)
+
+To compare VI outputs:
+1. Run VI_compare.py (chaging the filenames in the code to the ones you want to compare, usually one VI vs the truth table if truth exists)
+
+Also:
+1. VI_convert_v1pt1_to_v1pt2.py converts the old v1.1 from Rita to our common v1.2 format. 
+
--- VI_compare.py
+++ VI_compare.py
@@ -4,6 +4,7 @@ import os, sys, glob
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
+import fnmatch
 
 def plot_scatter_colors(x,y,col,clim,xlabel,ylabel,collabel,plot_name,xlim=0,ylim=0,psize=8.0,iextra=[9999],annotation=''):
   plt.figure(dpi=200)
@@ -35,62 +36,85 @@ def plot_histogram(x,bin_edges,xlabel,ylabel,plot_name,comp=[999],annotation='',
   plt.savefig(plot_name,bbox_inches='tight')
   plt.close()
 
+def compare_vi(VI_dir,VI_file,VI_truth_file,output_dir):
+  VI_base = VI_file[:-4]  # Get rid of .csv at the end of the filename so we can add .png etc... for figures and output. 
+  # Compare different visual inspections
+  m = pd.read_csv(VI_truth_file) #merged file or file you want to compare to
+  v = pd.read_csv(VI_dir+VI_file,delimiter = " , ", engine='python') #NOTE, prospect outputs data with spaces between commas, so you need the weird delimeter for prospect output, but not for normal csv output like we have done for the merged files
+  # Change the column name to TARGETID to match standards elsewhere in DESI.
+  v = v.rename(columns={"TargetID": "TARGETID"})
+
+  # If the VI agreed with Redrock, insert the Redrock results into the VI column.
+  v.loc[v['VI z']=='--', 'VI z'] = v.loc[v['VI z']=='--', 'Redrock z']
+  v['VI z'] = v['VI z'].astype(float)
+  v.loc[v['VI spectype']=='--', 'VI spectype'] = v.loc[v['VI spectype']=='--', 'Redrock spectype']
+
+  # Gather the results by TARGETID, put them all in g, including only those items the VI'er looked at.
+  g = v.merge(m,how='left',on='TARGETID')
+
+  # Find the disagreements
+  i_disagree_z = np.abs(g['VI z']-g['best z']) >=0.0033 
+  #print( g[['TARGETID','best z','VI z']][i_disagree_z])
+  #print(sum(i_disagree_z))
+
+  i_disagree_class = abs(g['VI class']-g['best quality']) >= 2  
+  #print( g[['TARGETID','best quality','VI class']][i_disagree_class])
+  #print(sum(i_disagree_class))
+
+  i_disagree_spectype = g['VI spectype'] != g['best spectype']
+  #print( g[['TARGETID','best spectype','VI spectype']][i_disagree_spectype])
+  #print(sum(i_disagree_spectype))
+
+  i_good_bestz = g['best quality'] >=2.5  # Choose only those redshifts that were given a quality >=2.5 in the merged results
+  i_good_VIz   = g['VI class']   >=3.0  # Choose only those redshifts that were given a quality >=3 by the VI
+  i_good_z = i_good_bestz | i_good_VIz
+
+  i_disagree_all = i_disagree_class | i_disagree_z | i_disagree_spectype
+  i_disagree_good = i_disagree_z & i_good_z
+  print('\nOut of %s objects, you disagreed with the merged results on %s redshifts, %s qualities, and %s spectypes.'%(len(g['TARGETID']),sum(i_disagree_z),sum(i_disagree_class),sum(i_disagree_spectype)))
+  print('\nNumber of redshift disagreements on high-quality redshifts = %s.'%sum(i_disagree_good))
+  print('\nTotal number of disagreements = %s.'%sum(i_disagree_all))
+  print( g[['TARGETID','best z','VI z','best quality','VI class','best spectype','VI spectype']][i_disagree_all])
+
+  conflicts = ''
+  for target in g['TARGETID'][i_disagree_all]:
+    conflicts = conflicts+', '+str(target)
+  print('\nLook at your disagreements by opening the Prospect notebook viewer https://github.com/desihub/prospect/blob/master/doc/nb/Prospect_TARGETID.ipynb/ and cutting and pasting the following target list:')
+  print(conflicts[2:])
+
+  # Plot VI z vs Merged z and colour by Quality
+  plot_scatter_colors(g['best z'],g['VI z'],g['best quality'],[0.0,4.0],'Best z','VI z','Quality (4 is good)',output_dir+VI_base+'_z.png',iextra=i_disagree_good,annotation='# of z disagreements on high-quality z: %s'%sum(i_disagree_good))
+  plot_histogram(g['best quality'],np.arange(11)/2.-0.25,'Quality','Number of cases',output_dir+VI_base+'_quality.png',comp=g['VI class'],annotation='Blue=Merged VI; Orange=Single VI')
+
+
 # Read in truth table (or any output file you'd like to compare to)
-#VI_truth = '/global/cfs/cdirs/desi/sv/vi/TruthTables/truth_table_QSO_tile68002_night20200315.csv'
 VI_dir     = '/Users/uqtdavi1/Documents/programs/DESI/SV/VI_files/SV0/QSO/'
-VI_merged  = 'output/desi-vi_SV0_QSO_tile68002_night20200315_1_merged.csv'
-who = 'TMD'
-VI_base    = 'desi-vi_SV0_QSO_tile68002_night20200315_1_'+who  # We add .csv to read in this file, and e.g. _z.png to print plots for this file
+#VI_truth_file = '/global/cfs/cdirs/desi/sv/vi/TruthTables/truth_table_QSO_tile68002_night20200315.csv'
+VI_truth_file  = VI_dir+'output/desi-vi_SV0_QSO_tile68002_night20200315_1_merged.csv'
 output_dir = VI_dir+'VI_check/'  # The directory in which we'll put the output. 
 if not os.path.exists(output_dir):
   os.makedirs(output_dir)
 
-m = pd.read_csv(VI_dir+VI_merged) 
-v = pd.read_csv(VI_dir+VI_base+'.csv',delimiter = " , ", engine='python') #NOTE, prospect outputs data with spaces between commas, so you need the weird delimeter for prospect output, but not for normal csv output like we have done for the merged files
-# Change the column name to TARGETID to match standards elsewhere in DESI.
-v = v.rename(columns={"TargetID": "TARGETID"})
-#print(m.keys())
-#print(v.keys())
+# Set this to true if you want to do a batch job!
+Compare_Everything_In_Directory = True
+# If you don't want to compare everytihng define a single file here. 
+VI_file    = 'desi-vi_SV0_QSO_tile68002_night20200315_1_TMD.csv'  # We add .csv to read in this file, and e.g. _z.png to print plots for this file
 
-# If the VI agreed with Redrock, insert the Redrock results into the VI column.
-v.loc[v['VI z']=='--', 'VI z'] = v.loc[v['VI z']=='--', 'Redrock z']
-v['VI z'] = v['VI z'].astype(float)
-v.loc[v['VI spectype']=='--', 'VI spectype'] = v.loc[v['VI spectype']=='--', 'Redrock spectype']
+if Compare_Everything_In_Directory:
+  # Find some VI files to check
+  all_files = os.listdir(VI_dir)
+  vi_files=[]
+  # Choose a subset
+  subset = '_1_'
+  pattern = "desi*"+subset+"*.csv"
+  for entry in all_files:
+    if fnmatch.fnmatch(entry, pattern):
+      vi_files.append(entry)
+else:
+  vi_files=[VI_file]
 
-# Gather the results by TARGETID, put them all in g, including only those items the VI'er looked at.
-g = v.merge(m,how='left',on='TARGETID')
 
-# Find the disagreements
-i_disagree_z = np.abs(g['VI z']-g['best z']) >=0.0033 
-#print( g[['TARGETID','best z','VI z']][i_disagree_z])
-#print(sum(i_disagree_z))
+print(vi_files)
 
-i_disagree_class = abs(g['VI class']-g['best quality']) >= 2  
-#print( g[['TARGETID','best quality','VI class']][i_disagree_class])
-#print(sum(i_disagree_class))
-
-i_disagree_spectype = g['VI spectype'] != g['best spectype']
-#print( g[['TARGETID','best spectype','VI spectype']][i_disagree_spectype])
-#print(sum(i_disagree_spectype))
-
-i_good_bestz = g['best quality'] >=2.5  # Choose only those redshifts that were given a quality >=2.5 in the merged results
-i_good_VIz   = g['VI class']   >=3.0  # Choose only those redshifts that were given a quality >=3 by the VI
-i_good_z = i_good_bestz | i_good_VIz
-
-i_disagree_all = i_disagree_class | i_disagree_z | i_disagree_spectype
-i_disagree_good = i_disagree_z & i_good_z
-print('\nOut of %s objects, you disagreed with the merged results on %s redshifts, %s qualities, and %s spectypes.'%(len(g['TARGETID']),sum(i_disagree_z),sum(i_disagree_class),sum(i_disagree_spectype)))
-print('\nNumber of redshift disagreements on high-quality redshifts = %s.'%sum(i_disagree_good))
-print('\nTotal number of disagreements = %s.'%sum(i_disagree_all))
-print( g[['TARGETID','best z','VI z','best quality','VI class','best spectype','VI spectype']][i_disagree_all])
-
-conflicts = ''
-for target in g['TARGETID'][i_disagree_all]:
-  conflicts = conflicts+', '+str(target)
-print('\nLook at your disagreements by opening the Prospect notebook viewer https://github.com/desihub/prospect/blob/master/doc/nb/Prospect_TARGETID.ipynb/ and cutting and pasting the following target list:')
-print(conflicts[2:])
-
-# Plot VI z vs Merged z and colour by Quality
-plot_scatter_colors(g['best z'],g['VI z'],g['best quality'],[0.0,4.0],'Best z','VI z','Quality (4 is good)',output_dir+VI_base+'_z.png',iextra=i_disagree_good,annotation='# of z disagreements on high-quality z: %s'%sum(i_disagree_good))
-plot_histogram(g['best quality'],np.arange(11)/2.-0.25,'Quality','Number of cases',output_dir+VI_base+'_quality.png',comp=g['VI class'],annotation='Blue=Merged VI; Orange=Single VI')
-
+for file in vi_files:
+  compare_vi(VI_dir,file,VI_truth_file,output_dir)

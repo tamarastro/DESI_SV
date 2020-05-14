adf9a080428e62b5d1d0050b0745ab74f26ec352
Tamara Davis <tamarad@physics.uq.edu.au>
2020-05-14 14:33:23 +1000

    Some edits to VI_compare.py to make it more user friendly.

--- VI_compare.py
+++ VI_compare.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt 
 import fnmatch
+import argparse
 
 def plot_scatter_colors(x,y,col,clim,xlabel,ylabel,collabel,plot_name,xlim=0,ylim=0,psize=8.0,iextra=[9999],annotation=''):
   plt.figure(dpi=200)
@@ -39,7 +40,7 @@ def plot_histogram(x,bin_edges,xlabel,ylabel,plot_name,comp=[999],annotation='',
 def compare_vi(VI_dir,VI_file,VI_truth_file,output_dir):
   VI_base = VI_file[:-4]  # Get rid of .csv at the end of the filename so we can add .png etc... for figures and output. 
   # Compare different visual inspections
-  m = pd.read_csv(VI_truth_file) #merged file or file you want to compare to
+  t = pd.read_csv(VI_truth_file) #truth (merged) file or file you want to compare to
   v = pd.read_csv(VI_dir+VI_file,delimiter = " , ", engine='python') #NOTE, prospect outputs data with spaces between commas, so you need the weird delimeter for prospect output, but not for normal csv output like we have done for the merged files
   # Change the column name to TARGETID to match standards elsewhere in DESI.
   v = v.rename(columns={"TargetID": "TARGETID"})
@@ -50,7 +51,7 @@ def compare_vi(VI_dir,VI_file,VI_truth_file,output_dir):
   v.loc[v['VI spectype']=='--', 'VI spectype'] = v.loc[v['VI spectype']=='--', 'Redrock spectype']
 
   # Gather the results by TARGETID, put them all in g, including only those items the VI'er looked at.
-  g = v.merge(m,how='left',on='TARGETID')
+  g = v.merge(t,how='left',on='TARGETID')
 
   # Find the disagreements
   i_disagree_z = np.abs(g['VI z']-g['best z']) >=0.0033 
@@ -88,19 +89,23 @@ def compare_vi(VI_dir,VI_file,VI_truth_file,output_dir):
 
 
 # Read in truth table (or any output file you'd like to compare to)
-VI_dir     = '/Users/uqtdavi1/Documents/programs/DESI/SV/VI_files/SV0/QSO/'
 #VI_truth_file = '/global/cfs/cdirs/desi/sv/vi/TruthTables/truth_table_QSO_tile68002_night20200315.csv'
-VI_truth_file  = VI_dir+'output/desi-vi_SV0_QSO_tile68002_night20200315_1_merged.csv'
+VI_truth_file  = '/Users/uqtdavi1/Documents/programs/DESI/SV/VI_files/SV0/QSO/output/desi-vi_SV0_QSO_tile68002_night20200315_1_merged.csv'
+
+# Define the VI file you want to compare, and the directory you find it in, and the output directory you want to send it to
+VI_file    =  'desi-vi_SV0_QSO_tile68002_night20200315_1_TMD.csv'   # We remove .csv and add e.g. _z.png to print plots for this file
+VI_dir     = '/Users/uqtdavi1/Documents/programs/DESI/SV/VI_files/SV0/QSO/'
 output_dir = VI_dir+'VI_check/'  # The directory in which we'll put the output. 
 if not os.path.exists(output_dir):
   os.makedirs(output_dir)
 
-# Set this to true if you want to do a batch job!
-Compare_Everything_In_Directory = True
-# If you don't want to compare everytihng define a single file here. 
-VI_file    = 'desi-vi_SV0_QSO_tile68002_night20200315_1_TMD.csv'  # We add .csv to read in this file, and e.g. _z.png to print plots for this file
+# Set this to True if you want to do a batch job!  
+# This will ignore the vi file you enter as VI_file above, but uses the directories and truth table.
+Compare_Everything_In_Directory = False
 
-if Compare_Everything_In_Directory:
+
+# Note, the compare everything in directory option is fragile... requires you to set the pattern you want below.
+if Compare_Everything_In_Directory:  
   # Find some VI files to check
   all_files = os.listdir(VI_dir)
   vi_files=[]
